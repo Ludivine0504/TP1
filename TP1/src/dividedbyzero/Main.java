a4ad7a8fa576a1c89cf46cd2dfcf24a13a3142cc
Hyanmo <yanmoo0807@163.com>
2024-03-19 16:58:13 +0800

    Exception specified

--- TP1/src/dividedbyzero/Main.java
+++ TP1/src/dividedbyzero/Main.java
@@ -4,18 +4,16 @@ public class Main {
  /**
  * @param args the command line arguments
  */
- public static void main(String[] args) {
- System.out.println(3/0);
-}
-
-	    
+     
 public static void main(String[] args) {
 	        try{
-	           //Block of code to try
+	           System.out.println(3/0);
 	        }catch (NullPointerException e){
 	        	//Print the error message like this:
 	        	System.err.println("NullPointerException:"+e.getMessage());
 	        	//Or handle the error another way here
+	        }catch (ArithmeticException e) {
+	        	System.err.println("ArithmeticException:"+e.getMessage());
 	        }
 	    }
 	}
